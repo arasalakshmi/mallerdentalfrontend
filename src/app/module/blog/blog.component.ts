d9ac4cc602e686abbdbcd6d174003b079190e3ec
Arasalakshmi <arasalakshmik@gmail.com>
2026-07-19 16:44:57 +0530

    added fixed

--- src/app/module/blog/blog.component.ts
+++ src/app/module/blog/blog.component.ts
@@ -46,4 +46,68 @@ export class BlogComponent implements OnInit {
       "@type": "CollectionPage",
       "@id": "https://mallerdentalfrontend.vercel.app/blog",
       "url": "https://mallerdentalfrontend.vercel.app/blog",
-      
+      "name": "Dental Blog | Maller Dental Clinic",
+      "description": "Expert dental articles, oral health tips, treatment guides and preventive dentistry information from Maller Dental Clinic in RS Puram, Coimbatore.",
+      "inLanguage": "en-IN",
+      "about": {
+        "@id": "https://mallerdentalfrontend.vercel.app/#dentist"
+      },
+      "breadcrumb": {
+        "@id": "https://mallerdentalfrontend.vercel.app/blog#breadcrumb"
+      }
+    },
+    {
+      "@type": "BreadcrumbList",
+      "@id": "https://mallerdentalfrontend.vercel.app/blog#breadcrumb",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "name": "Home",
+          "item": "https://mallerdentalfrontend.vercel.app"
+        },
+        {
+          "@type": "ListItem",
+          "position": 2,
+          "name": "Dental Blog",
+          "item": "https://mallerdentalfrontend.vercel.app/blog"
+        }
+      ]
+    },
+    {
+      "@type": "Dentist",
+      "@id": "https://mallerdentalfrontend.vercel.app/#dentist",
+      "name": "Maller Dental Clinic",
+      "url": "https://mallerdentalfrontend.vercel.app",
+      "telephone": "+91 94429 51313",
+      "priceRange": "₹₹",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "First Floor, Gowtham Arcade, 48, Thiruvenkatasamy Rd W, Opposite Corporation Kalaiarangam, R.S. Puram",
+        "addressLocality": "Coimbatore",
+        "addressRegion": "Tamil Nadu",
+        "postalCode": "641002",
+        "addressCountry": "IN"
+      },
+      "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": 11.0097234,
+        "longitude": 76.946896
+      },
+      "sameAs": [
+        "https://g.page/r/C..."
+      ]
+    }
+  ]
+})
+  }
+
+  loadBlogs() {
+  this.sanityService.getBlogs().then((data: any) => {
+    console.log('Blogs from Sanity:', data);
+    this.blogs = data;
+  }).catch(err => {
+    console.error('Sanity Error:', err);
+  });
+}
+}
